cee074510193c0d1c32e9eaf929d929aaeb7bbda
Cee <18570103+CeeIM@users.noreply.github.com>
2021-04-02 22:18:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/kenzok8/openwrt-packages cee
-git clone https://github.com/xiaorouji/openwrt-passwall passwall
+git clone https://github.com/kenzok8/openwrt-packages package/cee
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
